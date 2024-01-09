ea27ff7215a5aa94570c4d48526b4fd27b4f466a
Mukilan003 <121660765+Mukilan003@users.noreply.github.com>
2024-01-10 00:25:06 +0530

    Add files via upload

---
+++ TASK-3/app.js
@@ -0,0 +1,373 @@
+// 1. ============================================================================================================
+
+
+// let buttons =  document.querySelectorAll('button')
+
+// let display  = document.querySelector('.display')
+
+// buttons.forEach(function(button){
+//     button.addEventListener("click", calculate)
+// })
+
+// function  calculate(event){
+//     const buttonclickValue = event.target.value
+//     if(buttonclickValue === "="){
+//         if(display.value !==""){
+//             display.value = eval(display.value)
+//         }
+//     }else if(buttonclickValue === "C"){
+//         display.value = ""
+//     }else{
+//         display.value += buttonclickValue
+//     }
+// }
+
+
+// 2. ============================================================================================================
+
+// let display = document.querySelector(".display")
+
+// let buttons = document.querySelectorAll("button")
+
+// buttons.forEach(function(button){
+//     button.addEventListener("click", calculate)
+// })
+
+// function calculate(event){
+//     const buttonclickValue = event.target.value
+//     if(buttonclickValue === "="){
+//         if(display.value !== ""){
+//             display.value = eval(display.value)
+//         }
+//     }else if(buttonclickValue === "C"){
+//         display.value = ""
+//     }else{
+//         display.value += buttonclickValue
+//     }
+// }
+
+
+// 3. ============================================================================================================
+
+
+// let display = document.querySelector(".display")
+
+// let buttons = document.querySelectorAll("button")
+
+// buttons .forEach(function(button){
+//     button.addEventListener("click", calculate)
+// })
+
+// function calculate(event){
+//     const buttonclickValue = event.target.value
+//     if(buttonclickValue === "="){
+//         if(display.value !== ""){
+//             display.value = eval(display.value)
+//         }
+//     }else if(buttonclickValue === "C"){
+//         display.value = ""
+//     }else{
+//         display.value += buttonclickValue
+//     }
+// }
+
+
+
+//4. ============================================================================================================
+
+// let display = document.querySelector(".display")
+
+// let buttons = document.querySelectorAll("button")
+
+// buttons .forEach(function(button){
+//     button.addEventListener("click", calculate)
+// })
+
+// function calculate(event){
+//     const buttonclickValue = event.target.value
+//     if(buttonclickValue === "="){
+//         if(display.value !== ""){
+//             display.value = eval(display.value)
+//         }
+//     }else if(buttonclickValue === "C"){
+//         display.value = ""
+//     }else{
+//         display.value += buttonclickValue
+//     }
+// }
+
+//5. ============================================================================================================
+
+// let display = document.querySelector(".display")
+
+// let buttons = document.querySelectorAll("button")
+
+// buttons .forEach(function(button){
+//     button.addEventListener("click", calculate)
+// })
+
+// function calculate(event){
+//     const buttonclickValue = event.target.value
+//     if(buttonclickValue === "="){
+//         if(display.value !== ""){
+//             display.value = eval(display.value)
+//         }
+//     }else if(buttonclickValue === "C"){
+//         display.value = ""
+//     }else{
+//         display.value += buttonclickValue
+//     }
+// }
+
+//6. ============================================================================================================
+
+// let display = document.querySelector(".display")
+
+// let buttons = document.querySelectorAll("button")
+
+// buttons .forEach(function(button){
+//     button.addEventListener("click", calculate)
+// })
+
+// function calculate(event){
+//     const buttonclickValue = event.target.value
+//     if(buttonclickValue === "="){
+//         if(display.value !== ""){
+//             display.value = eval(display.value)
+//         }
+//     }else if(buttonclickValue === "C"){
+//         display.value = ""
+//     }else{
+//         display.value += buttonclickValue
+//     }
+// }
+
+//8. ============================================================================================================
+
+// let display = document.querySelector(".display")
+
+// let buttons = document.querySelectorAll("button")
+
+// buttons .forEach(function(button){
+//     button.addEventListener("click", calculate)
+// })
+
+// function calculate(event){
+//     const buttonclickValue = event.target.value
+//     if(buttonclickValue === "="){
+//         if(display.value !== ""){
+//             display.value = eval(display.value)
+//         }
+//     }else if(buttonclickValue === "C"){
+//         display.value = ""
+//     }else{
+//         display.value += buttonclickValue
+//     }
+// }
+
+//8. ============================================================================================================
+
+// let display = document.querySelector(".display")
+
+// let buttons = document.querySelectorAll("button")
+
+// buttons .forEach(function(button){
+//     button.addEventListener("click", calculate)
+// })
+
+// function calculate(event){
+//     const buttonclickValue = event.target.value
+//     if(buttonclickValue === "="){
+//         if(display.value !== ""){
+//             display.value = eval(display.value)
+//         }
+//     }else if(buttonclickValue === "C"){
+//         display.value = ""
+//     }else{
+//         display.value += buttonclickValue
+//     }
+// }
+
+//9. ============================================================================================================
+
+// let display = document.querySelector(".display")
+
+// let buttons = document.querySelectorAll("button")
+
+// buttons .forEach(function(button){
+//     button.addEventListener("click", calculate)
+// })
+
+// function calculate(event){
+//     const buttonclickValue = event.target.value
+//     if(buttonclickValue === "="){
+//         if(display.value !== ""){
+//             display.value = eval(display.value)
+//         }
+//     }else if(buttonclickValue === "C"){
+//         display.value = ""
+//     }else{
+//         display.value += buttonclickValue
+//     }
+// }
+
+//10. ============================================================================================================
+
+// let display = document.querySelector(".display")
+
+// let buttons = document.querySelectorAll("button")
+
+// buttons .forEach(function(button){
+//     button.addEventListener("click", calculate)
+// })
+
+// function calculate(event){
+//     const buttonclickValue = event.target.value
+//     if(buttonclickValue === "="){
+//         if(display.value !== ""){
+//             display.value = eval(display.value)
+//         }
+//     }else if(buttonclickValue === "C"){
+//         display.value = ""
+//     }else{
+//         display.value += buttonclickValue
+//     }
+// }
+
+//11. ============================================================================================================
+
+// let display = document.querySelector(".display")
+
+// let buttons = document.querySelectorAll("button")
+
+// buttons .forEach(function(button){
+//     button.addEventListener("click", calculate)
+// })
+
+// function calculate(event){
+//     const buttonclickValue = event.target.value
+//     if(buttonclickValue === "="){
+//         if(display.value !== ""){
+//             display.value = eval(display.value)
+//         }
+//     }else if(buttonclickValue === "C"){
+//         display.value = ""
+//     }else{
+//         display.value += buttonclickValue
+//     }
+// }
+
+//12. ============================================================================================================
+
+// let display = document.querySelector(".display")
+
+// let buttons = document.querySelectorAll("button")
+
+// buttons .forEach(function(button){
+//     button.addEventListener("click", calculate)
+// })
+
+// function calculate(event){
+//     const buttonclickValue = event.target.value
+//     if(buttonclickValue === "="){
+//         if(display.value !== ""){
+//             display.value = eval(display.value)
+//         }
+//     }else if(buttonclickValue === "C"){
+//         display.value = ""
+//     }else{
+//         display.value += buttonclickValue
+//     }
+// }
+
+//13. ============================================================================================================
+
+// let display = document.querySelector(".display")
+// let buttons = document.querySelectorAll("button")
+// buttons .forEach(function(button){
+//     button.addEventListener("click", calculate)
+// })
+
+// function calculate(event){
+//     const buttonclickValue = event.target.value
+//     if(buttonclickValue === "="){
+//         if(display.value !== ""){
+//             display.value = eval(display.value)
+//         }
+//     }else if(buttonclickValue === "C"){
+//         display.value = ""
+//     }else{
+//         display.value += buttonclickValue
+//     }
+// }
+
+//14. ============================================================================================================
+
+// let display = document.querySelector(".display")
+// let buttons = document.querySelectorAll("button")
+
+// buttons .forEach(function(button){
+//     button.addEventListener("click", calculate)
+// })
+
+// function calculate(event){
+//     const buttonclickValue = event.target.value
+//     if(buttonclickValue === "="){
+//         if(display.value !== ""){
+//             display.value = eval(display.value)
+//         }
+//     }else if(buttonclickValue === "C"){
+//         display.value = ""
+//     }else{
+//         display.value += buttonclickValue
+//     }
+// }
+
+
+//15. ============================================================================================================
+
+
+// let display = document.querySelector(".display")
+// let buttons = document.querySelectorAll("button")
+
+// buttons .forEach(function(button){
+//     button.addEventListener("click", calculate)
+// })
+
+// function calculate(event){
+//     const buttonclickValue = event.target.value
+//     if(buttonclickValue === "="){
+//         if(display.value !== ""){
+//             display.value = eval(display.value)
+//         }
+//     }else if(buttonclickValue === "C"){
+//         display.value = ""
+//     }else{
+//         display.value += buttonclickValue
+//     }
+// }
+
+//16. ============================================================================================================
+
+
+let display = document.querySelector(".display")
+let buttons = document.querySelectorAll("button")
+
+buttons .forEach(function(button){
+    button.addEventListener("click", calculate)
+})
+
+function calculate(event){
+    const buttonclickValue = event.target.value
+    if(buttonclickValue === "="){
+        if(display.value !== ""){
+            display.value = eval(display.value)
+        }
+    }else if(buttonclickValue === "C"){
+        display.value = ""
+    }else{
+        display.value += buttonclickValue
+    }
+}
+
+//17. ============================================================================================================
